1630c1ab139502b834098c7571b45f98d05d7484
Haen <haen@mdaqs-MacBook-Pro-7.local>
2018-07-23 17:59:56 +0800

    changing KRW selection name and toAmount calculation logic

--- popup.js
+++ popup.js
@@ -1,31 +1,6 @@
-var rates = [1.3662, 1130.30, 827.23];
-
-var USDSGD = rates[0];
-var USDKRW = rates[1];
-var SGDKRW = rates[2];
-
 var result = 0;
 var data = null;
 var api = API();
-
-var default_date_format = 'YYYY-MM-DD';
-
-var fromCcy = document.getElementById('from');
-var fromAmt = document.getElementById('fromAmount');
-var toCcy = document.getElementById('to');
-var toAmount = document.getElementById('toAmount');
-
-fromAmt.addEventListener("keyup", function(){
-  convert();
-});
-
-fromCcy.addEventListener("change", function(){
-  convert();
-});
-
-toCcy.addEventListener("change", function(){
-  convert();
-});
 
 function request() {
   var current = moment().format(default_date_format);
@@ -57,25 +32,29 @@ function init() {
   setInterval(request, 1000 * 60 * 60);
 }
 
-function convert() {
-  if(fromCcy.value == 'USD' && toCcy.value == 'SGD') {
-    result = fromAmt.value * USDSGD;
-  } else if (fromCcy.value == 'SGD' && toCcy == 'USD'){
-    result = fromAmt.value / USDSGD;
-  } else if (fromCcy.value == 'USD' && toCcy.value == 'KRW'){
-    result = fromAmt.value * USDKRW;
-  } else if (fromCcy.value == 'KRW' && toCcy.value == 'USD'){
-    result = fromAmt.value / USDKRW;
-  } else if (fromCcy.value == 'SGD' && toCcy.value == 'KRW'){
-    result = fromAmt.value * SGDKRW;
-  } else if (fromCcy.value == 'KRW' && toCcy.value == 'SGD'){
-    result = fromAmt.value / SGDKRW;
-  } else {
-    alert("Invalid input");
-  }
-  
-  toAmount.value = result;
-  return result
-}
 
 init();
+
+
+var default_date_format = 'YYYY-MM-DD';
+
+var fromCcy = document.getElementById('from');
+var fromAmt = document.getElementById('fromAmount');
+var toCcy = document.getElementById('to');
+var toAmount = document.getElementById('toAmount');
+
+fromAmt.addEventListener("keyup", function(){
+  convert();
+});
+
+fromCcy.addEventListener("change", function(){
+  convert();
+});
+
+toCcy.addEventListener("change", function(){
+  convert();
+});
+
+function convert() {
+  toAmount.value = Number(Math.round((fromAmt.value / data.rates[fromCcy.value] * data.rates[toCcy.value])*100)/100).toLocaleString('en');
+}
